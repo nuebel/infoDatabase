89c37e965d48b98f8c42cd6e5f89d3f44ee41a9d
Nic Uebel <nic.uebel@gmail.com>
2020-02-28 09:32:10 -0500

    Removed specific database info.

--- includes/db_open.php
+++ includes/db_open.php
@@ -1,9 +1,9 @@
 <?php
     require_once('DB.php');
-    $mydbhost = "database.hosting.vt.edu";
-    $mydatabase = "database372";
-    $mydbuserid = "site372";
-    $mydbpassword = "WAZJEFFF";
+    $mydbhost = "__DBHOST__";
+    $mydatabase = "__DB__";
+    $mydbuserid = "__DBUSER__";
+    $mydbpassword = "__DBPW__";
     $database = DB::connect( "mysql://".$mydbuserid.":".$mydbpassword."@".$mydbhost."/".$mydatabase );
     if (PEAR::isError($database)) {echo "Error."; die($database->getMessage());}
 ?>
